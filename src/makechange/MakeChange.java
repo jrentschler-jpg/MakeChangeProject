87cf85db10d6768e1ecf1bec2990d6462450a74c
Jourdan Rentschler <jrentschler3@gmail.com>
2020-08-28 16:48:20 -0600

    my first commit

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,8 +1,16 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner.kb = new Scanner (System.in);
+		
+		System.out.println("Your Total Amount is: " + amount);
+		System.out.println("You have given: "+ tendered);
+		System.out.println("You exact change amount is: "+ result);
+		
 		
 
 	}
